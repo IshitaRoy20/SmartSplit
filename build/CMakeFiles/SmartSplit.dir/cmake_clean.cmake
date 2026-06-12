ffb19e6e00c3fa4e82982c9c961ef10cf18c6eb8
ISHITAaa <ishitaroy20042005@gmail.com>
2026-06-12 18:41:46 +0000

    feat: initialize SmartSplit CLI architecture and group management

---
+++ build/CMakeFiles/SmartSplit.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartSplit.dir/src/cli/Menu.cpp.o"
+  "CMakeFiles/SmartSplit.dir/src/cli/Menu.cpp.o.d"
+  "CMakeFiles/SmartSplit.dir/src/main.cpp.o"
+  "CMakeFiles/SmartSplit.dir/src/main.cpp.o.d"
+  "SmartSplit"
+  "SmartSplit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartSplit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
