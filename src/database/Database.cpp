27e40250d97ce52937a1d8cce27a29cc9155c365
ISHITAaa <ishitaroy20042005@gmail.com>
2026-06-30 22:27:33 +0000

    feat:created database tables

--- src/database/Database.cpp
+++ src/database/Database.cpp
@@ -55,5 +55,77 @@ Database::getConnection()
 
 void Database::initialize()
 {
+    const char* sql = R"(
 
+    CREATE TABLE IF NOT EXISTS groups
+    (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS members
+    (
+        id INTEGER PRIMARY KEY,
+        group_id INTEGER NOT NULL,
+        name TEXT NOT NULL,
+
+        FOREIGN KEY(group_id)
+        REFERENCES groups(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS expenses
+    (
+        id INTEGER PRIMARY KEY,
+        group_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        amount REAL NOT NULL,
+
+        FOREIGN KEY(group_id)
+        REFERENCES groups(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS expense_payments
+    (
+        expense_id INTEGER NOT NULL,
+        member_id INTEGER NOT NULL,
+        amount_paid REAL NOT NULL,
+
+        FOREIGN KEY(expense_id)
+        REFERENCES expenses(id),
+
+        FOREIGN KEY(member_id)
+        REFERENCES members(id)
+    );
+
+    )";
+
+    char* errorMessage = nullptr;
+
+    int result =
+        sqlite3_exec(
+            db,
+            sql,
+            nullptr,
+            nullptr,
+            &errorMessage
+        );
+
+    if(result != SQLITE_OK)
+    {
+        std::cout
+            << "Database Initialization Failed\n";
+
+        std::cout
+            << errorMessage
+            << "\n";
+
+        sqlite3_free(
+            errorMessage
+        );
+
+        return;
+    }
+
+    std::cout
+        << "Database Tables Ready\n";
 }
